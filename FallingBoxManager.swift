a832f4f09fc0fa21fa18d631f7ef0eed694fad67
andrew-rios <andrewrobertrios@gmail.com>
2023-11-03 23:56:13 -0400

    added boxmanger

---
+++ FallingBoxManager.swift
@@ -0,0 +1,8 @@
+//
+//  FallingBoxManager.swift
+//  Game2
+//
+//  Created by Andrew Rios on 10/22/23.
+//
+
+import Foundation
---
+++ Game2/BoxManager.swift
@@ -0,0 +1,8 @@
+//
+//  BoxManager.swift
+//  Game2
+//
+//  Created by Andrew Rios on 10/21/23.
+//
+
+import Foundation
